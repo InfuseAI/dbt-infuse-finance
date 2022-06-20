e3e1c4efe53f3b79b2bfd8f735c029e4fd8a7fb4
Gabriel Lo <gabriel@InfuseAI-M1Pro.local>
2022-06-20 09:20:20 +0900

    modified PRICE_PRESENT.sql

--- models/PRICE_PRESENT.sql
+++ models/PRICE_PRESENT.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
-select * from {{ source('PUBLIC', 'PRICE') }} where PRICE.DATE >= '2021-01-01' and PRICE.DATE < '2022-04-01'
+select * from {{ source('PUBLIC', 'PRICE') }} where PRICE.DATE >= '2021-01-01'
 
